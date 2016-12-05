c8bab2f2739ae41a4b9174087ee6b0cec0693afe
Sam Bruno <sjbrunster@gmail.com>
2016-12-05 18:06:29 +0000

    Fix js template function

--- app/assets/javascripts/madlibs.js
+++ app/assets/javascripts/madlibs.js
@@ -1,9 +1,9 @@
 var template = function(i) {
   return "<li>" +
-  " <label for='song_lyrics_attributes_" + i + "_Word or Phrase'>Word or phrase</label>" +
-  '<input type="text" name="song[lyrics_attributes][' + i + '][word]" id="song_lyrics_attributes_${i}_word">' +
+  ' <label for="song_lyrics_attributes_' + i + '_Word or Phrase">Word or phrase</label>' +
+  '<input type="text" name="song[lyrics_attributes][' + i + '][word]" id="song_lyrics_attributes_' + i +'_word">' +
   '<label for="song_lyrics_attributes_' + i + '_part_of_speech">Part of speech</label>' +
-  '<input type="text" name="song[lyrics_attributes]' + i + '[part_of_speech]" id="song_lyrics_attributes_${i}_part_of_speech">'
+  '<input type="text" name="song[lyrics_attributes]' + i + '[part_of_speech]" id="song_lyrics_attributes_' + i + '_part_of_speech">' +
   '</li>';
 };
 
